6b19d17c29cc2035a178ac7fb3e7a6cbfd7e4a5c
YuliyaKopichenko <j.titorenko@gmail.com>
2021-11-02 11:52:10 +0300

    Add finally solution

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //! Реализуем Singleton через замыкание
 
 function Universe() {
-
+  
   // если при инициализации нового экземпляра у нас уже существует флаг exists(т.е. мы его ранее проинициализировали)
   // то мы останавливаем процесс создания нового instance и возвращаем старый instance
-  
+    
   if (Universe.exists) {
     return Universe.instance
   }
 
   Universe.instance = this; // заносим в поле instance контекст функции конструктора
   Universe.exists = true; // если true, то уже был проинициализирован
-  
+
   this.size = 100;
-  
-  this.getSize = function() {
+}
+
+Universe.prototype.getSize = function() {
     return this.size;
-  }
-  
-  this.setSize = function(value) {
+}
+
+Universe.prototype.setSize = function(value) {
     this.size = value;
-  }
 }
 
 let a = new Universe();
@@ -33,3 +33,6 @@ a.setSize(200);
 console.log(a.getSize(), b.getSize());
 
 console.log(`a === b ? ${a === b}`)
+
+
+// 2 объекта a и b ссылаются на 1 объект singlton
