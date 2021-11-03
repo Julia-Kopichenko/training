48e2e7875cde5905281dbc9b6159486dc86fd969
YuliyaKopichenko <j.titorenko@gmail.com>
2021-11-03 11:10:26 +0300

    fix solutions

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const buttonPlus = document.querySelector('.button-plus');
 const buttonMinus = document.querySelector('.button-minus');
 const counter = document.querySelector('.counter__value');
 
-buttonPlus.addEventListener('click', increaseCounter);
-buttonMinus.addEventListener('click', decreaseCounter);
+const increaseCounter = () => counter.innerHTML = +counter.innerHTML + 1;
 
-function increaseCounter() {
-  counter.innerHTML++;
-}
-function decreaseCounter() {
-  if(counter.innerHTML > 0) {
-    counter.innerHTML--;
+const decreaseCounter = () => {
+  if (+counter.innerHTML > 0) {
+    counter.innerHTML = +counter.innerHTML - 1;
   }
 }
+
+buttonPlus.addEventListener('click', increaseCounter);
+
+buttonMinus.addEventListener('click', decreaseCounter);
