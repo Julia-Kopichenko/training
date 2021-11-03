f2e750f25a05a9f1a44a8de34b190343f7e8df73
YuliyaKopichenko <j.titorenko@gmail.com>
2021-11-03 12:06:38 +0300

    Change innerHtml to innetText

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const buttonPlus = document.querySelector('.button-plus');
 const buttonMinus = document.querySelector('.button-minus');
 const counter = document.querySelector('.counter__value');
 
-const increaseCounter = () => counter.innerHTML = +counter.innerHTML + 1;
+const increaseCounter = () => counter.innerText = +counter.innerText + 1;
 
 const decreaseCounter = () => {
-  if (+counter.innerHTML > 0) {
-    counter.innerHTML = +counter.innerHTML - 1;
+  if (+counter.innerText > 0) {
+    counter.innerText = +counter.innerText - 1;
   }
 }
 
