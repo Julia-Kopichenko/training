1b558d58222baf9b17ad67843abdcc9561a636bb
YuliyaKopichenko <j.titorenko@gmail.com>
2021-11-03 15:14:55 +0300

    add solusion task2

--- task1/script.js
+++ task1/script.js
@@ -1,4 +1,13 @@
 const arr = [1,45,-9,6,6,67,2,3,71,70,14];
+const arr1 = [1,3];
+
+
+//! 1
+/**
+ * Создайте массив из 10 чисел. 
+ * Найдите количество элементов массива,
+ * которые отличны от наибольшего не более чем на 10%.
+ */
 
 function findCountElementsLessMax(arr) {
   const maxNumber = Math.max(...arr);
@@ -6,3 +15,16 @@ function findCountElementsLessMax(arr) {
 }
 
 console.log(findCountElementsLessMax(arr));
+
+//! 2
+/**
+ * Создайте массив из 10 чисел. 
+ * Найдите и выведите на экран наименьший четный элемент массива.
+ */
+
+const findEvenNumbers = (arr) => arr.filter(num => num % 2 === 0);
+
+const findMinNumber = (arr) => (arr.length !== 0) ? Math.min(...arr) : null;
+   
+console.log(findMinNumber(findEvenNumbers(arr)));
+
