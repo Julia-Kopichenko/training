71597632bcd521d2a9264cdc8392496a9af81c07
YuliyaKopichenko <j.titorenko@gmail.com>
2021-11-10 13:11:48 +0300

    fix solusion

--- promises_task3/script.js
+++ promises_task3/script.js
@@ -1,29 +1,23 @@
 const usersList = document.querySelector('.users')
 const requestURL = 'https://reqres.in/api/users?page=1';
 
-async function getUsers(url) {
+async function getData(url) {
   try {
     const response = await fetch(url);
-    if (response.ok) {
-      const {data: users} = await response.json();
-      return users;
-    } else {
-      console.log('Fetch Error :-S', err);  
-      return null;
-    }
+    const {data} = await response.json();
+    return data;
   } catch (error) {
     console.log('Fetch Error :-S', err);  
     return null;
   }
 }
 
-async function createUsersCards(response) {
-  let users = await response;
-  if (!users) {
+function createUsersCards(data) {
+  if (!data) {
     usersList.innerText = "Error, can't get data from server"
     return
   }
-  users.forEach(element => {
+  data.forEach(element => {
     const {first_name: name, email, avatar} = element;
     let user = document.createElement('li');
     user.classList.add('user');
@@ -35,4 +29,28 @@ async function createUsersCards(response) {
     usersList.appendChild(user) 
   });
 }
-createUsersCards(getUsers(requestURL))
+
+getData(requestURL)
+  .then(createUsersCards)
+
+//! Вариант 2
+// function createUsersCards(url) {
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(({data:users}) => {
+//       users.forEach(element => {
+//         const {first_name: name, email, avatar} = element;
+//         let user = document.createElement('li');
+//         user.classList.add('user');
+//         user.innerHTML = `
+//           <p class = 'user__name'>${name}</p>
+//           <p class = 'user__email'>${email}</p>
+//           <img src = ${avatar} alt='${name}' class='user__photo'>
+//         `;
+//         usersList.appendChild(user) 
+//       });
+//     })
+//     .catch(() => usersList.innerText = "Error, can't get data from server")
+// }
+
+// createUsersCards(requestURL)
